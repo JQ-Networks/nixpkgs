70a4f1ec31c1cb1b99bc903110b04a4be833993f
JQ-Networks-DevOps <devops@jq.mk>
2021-05-09 04:33:59 +0000

    v2ray exporter

--- pkgs/servers/monitoring/prometheus/v2ray-exporter.nix
+++ pkgs/servers/monitoring/prometheus/v2ray-exporter.nix
@@ -1,11 +1,31 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, nixosTests }:
+{ lib, stdenv, buildGoModule, fetchFromGitHub, nixosTests }:
 
-buildGoPackage rec {
-  pname = "v2ray_exporter";
+# buildGoModule rec {
+#   name = "v2ray-exporter";
+#   rev = "6d12e69bdeb9a34ff687f78ed1a695ad996242f9";
+#   # goPackagePath = "github.com/abowloflrf/v2ray_exporter";
+
+
+#   src = fetchFromGitHub {
+#     rev = rev;
+#     owner = "abowloflrf";
+#     repo = "v2ray-exporter";
+#     sha256 = "0iqsfwz74lw4zbzqxqnhw71rps96a6nhkgcjk0ln6zsdwzwlwqcg";
+#   };
+
+#   vendorSha256 = "1raknwgblv42dm2jx5vvbjdddhq5l0s9z1mmhhklv15l0j8hi3ji";
+
+#   meta = with stdenv.lib; {
+#     description = "V2Ray Prometheus Exporter";
+#     homepage = "https://github.com/abowloflrf/v2ray_exporter";
+#     # license = licenses.mit; # no license???
+#     maintainers = with maintainers; [ ];
+#   };
+# }
+
+buildGoModule rec {
+  pname = "v2ray-exporter";
   version = "0.5.1";
-
-  goPackagePath = "github.com/wi1dcard/v2ray-exporter";
-
 
   src = fetchFromGitHub {
     rev = "v${version}";
@@ -14,10 +34,12 @@ buildGoPackage rec {
     sha256 = "1dqiyy3mj1pid97d986yvz93d80kki71fk1afzzpzkbnas2cd4s3";
   };
 
+  vendorSha256 = "048bc48nvh4aw4iv20nk99ggmp0db8yb0rziljg6l6zjjvilwdrp";
+
   meta = with stdenv.lib; {
     description = "V2Ray Prometheus Exporter";
     homepage = "https://github.com/wi1dcard/v2ray-exporter";
-    license = licenses.MIT;
+    license = licenses.mit;
     maintainers = with maintainers; [ ];
   };
 }
